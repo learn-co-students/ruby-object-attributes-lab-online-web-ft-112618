49cb2ac5f644afd56c4151e9d3d3ad608dd6544f
Don Ward <10is0420@gmail.com>
2018-12-19 23:05:58 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+  
+  def name= (person_name)
+    
+    @name =  person_name
+    
+  end
+  
+  def name
+    
+    @name
+    
+  end
+  
+  def job= (job_label)
+    
+    @job = job_label
+    
+  end
+  
+  def job
+    
+    @job
+    
+  end
+  
+end
